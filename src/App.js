87c328865f8031250abf0f71889aac6a47d696a2
Your Name-asfasasfasf <you@example.com>
2022-08-17 07:36:02 +0500

    movies fetched from tmdb

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Row from './Row';
+import requests from './requests';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hey clever programmers! lets build netflix clone</h1>
+      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      {/* <Row title='NERFLIX ORIGINALS' /> */}
+
+
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  // A snippet of code which runs based on a condition/variable
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      // iT WILL DO THIS "https://api.themoviedb.org/3//discover/tv?api_key=${API_KEY}&with_netwoks=213"
+      // console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.log(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {/* several row__poster(s) */}
+
+        {movies.map((movie) => {
+          return (
+            <img
+              className="row__poster"
+              src={`${base_url}${movie.poster_path}`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
+
+      {/* {movies.map((movie)=>{ return <img key={movie.id} className="row__poster" src={`${base_url}${movie.backdrop_path}`} alt={movie.name}/> })} */}
+    </div>
+  );
+}
+
+export default Row;
