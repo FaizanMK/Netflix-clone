314e6f0989480dc6bc4ad44ed73cf06db1960149
Your Name-asfasasfasf <you@example.com>
2022-08-18 06:24:33 +0500

    Row work has been completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import './App.css';
-import Row from './Row';
-import requests from './requests';
-
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey clever programmers! lets build netflix clone</h1>
-      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
-      {/* <Row title='NERFLIX ORIGINALS' /> */}
+    <div className="app">
+      <Banner />
 
-
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+        //by default isLargeRow is true i.e. isLargeRow={true} but we don't have to write it
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import axios from "./axios";
 import "./Row.css";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   // A snippet of code which runs based on a condition/variable
@@ -29,8 +29,11 @@ function Row({ title, fetchUrl }) {
         {movies.map((movie) => {
           return (
             <img
-              className="row__poster"
-              src={`${base_url}${movie.poster_path}`}
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
               alt={movie.name}
             />
           );
