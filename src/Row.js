171fefea17f425218775ba772ea3b3947cab395d
Your Name-asfasasfasf <you@example.com>
2022-08-20 18:05:08 +0500

    adding trailer pop-ups

--- src/Row.js
+++ src/Row.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // A snippet of code which runs based on a condition/variable
   useEffect(() => {
@@ -19,6 +23,27 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
   // console.log(movies);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
   return (
     <div className="row">
@@ -29,6 +54,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => {
           return (
             <img
+              onClick={() => handleClick(movie)}
               key={movie.id}
               className={`row__poster ${isLargeRow && "row__posterLarge"}`}
               src={`${base_url}${
@@ -37,10 +63,11 @@ function Row({ title, fetchUrl, isLargeRow }) {
               alt={movie.name}
             />
           );
+
+          /* {movies.map((movie)=>{ return <img key={movie.id} className="row__poster" src={`${base_url}${movie.backdrop_path}`} alt={movie.name}/> })} */
         })}
       </div>
-
-      {/* {movies.map((movie)=>{ return <img key={movie.id} className="row__poster" src={`${base_url}${movie.backdrop_path}`} alt={movie.name}/> })} */}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}{" "}
     </div>
   );
 }
